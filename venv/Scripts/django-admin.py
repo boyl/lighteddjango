9102c7d6ba14ba1863418f1747513bb6d3e76c5a
boyl <1228417956@qq.com>
2019-03-13 18:03:02 +0800

    rm venv and add .gitignore

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!f:\works\study\workspace\lightteddjango\lighteddjango\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
